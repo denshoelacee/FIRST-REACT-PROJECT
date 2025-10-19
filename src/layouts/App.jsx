6d768db92f5cbd29a2c823adb01982bbbe80d661
Densho <sgwykdn@gmail.com>
2025-10-20 00:59:42 +0800

    case

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import Home from "../pages/Home";
+import Home from "../pages/home";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import Projects from "../pages/Projects";
-import Experiences from "../pages/Experiences";
-import Skills from "../pages/Skills";
-import Contacts from "../pages/Contacts";
+import Projects from "../pages/projects";
+import Experiences from "../pages/experiences";
+import Skills from "../pages/skills";
+import Contacts from "../pages/contacts";
 
 function App() {
   useEffect(() => {
